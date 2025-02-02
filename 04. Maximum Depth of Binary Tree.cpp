0904f4052b950269c9253b063df9c20fa22a2e79
rameshadigakunjal <153833038+rameshadigakunjal@users.noreply.github.com>
2025-02-02 17:09:52 +0530

    Update 04. Maximum Depth of Binary Tree.cpp

--- 04. Maximum Depth of Binary Tree.cpp
+++ 04. Maximum Depth of Binary Tree.cpp
@@ -8,6 +8,11 @@ A binary tree's maximum depth is the number of nodes along the longest path from
 
 Example 1:
 
+            3
+          /   \
+         9     20
+             /    \
+            15     7
 
 Input: root = [3,9,20,null,null,15,7]
 Output: 3
